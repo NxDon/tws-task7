ed06df2e47f4aa0fe700bfc5a0bd27ffc81f6b16
NxDon <645104928@qq.com>
2017-08-09 10:39:46 +0800

    finish

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -6,19 +6,47 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
-var main = require("../lib/main.js");
+var song = require("../lib/main.js");
 
 
-describe("测试描述", function(){
+describe("beerSong test", function(){
     sinon.spy(console, 'log');
-
-        it("return a lyric", function(){
-
-        var result = main();
-        var expect_string = '';
-
+        it("return a lyric when pass 0", function(){
+        var result = song(0);
+        var expect_string = `No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
         expect(expect_string).to.equal(result);
     });
+    it("return a lyric when pass 1", function(){
+        var result = song(1);
+        var expect_string = `1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
+        expect(expect_string).to.equal(result);
+    });
+    it("return a lyric when pass 2", function(){
+        var result = song(2);
+        var expect_string = `2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
+        expect(expect_string).to.equal(result);
+    });
+    it("return a lyric when pass 2", function(){
+        var result = song(2);
+        var expect_string = `2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
+        expect(expect_string).to.equal(result);
+    });
+
+
 
 
 });
